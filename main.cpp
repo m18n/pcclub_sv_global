f64f98f63b311ca1f77713443c3b1bfd2be1344f
Misha <20xim04@gmail.com>
2023-04-03 16:42:22 +0300

    Init

--- main.cpp
+++ main.cpp
@@ -1,13 +1,16 @@
 #include "crow.h"
-
+#include<iostream>
 int main()
 {
     crow::SimpleApp app;
 
     CROW_ROUTE(app, "/")
-    ([]() {
-        return "Hello, world! PIDAR";
+    ([](const crow::request& req, crow::response& res) {
+        
+        std::cout<<req.remote_ip_address<<"\n";
+        res.end();
+        
     });
-
+    
     app.port(9999).run();
 }
