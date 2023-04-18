6a0ba7e9d6f871fc41ad0f57b3c560e9d1b60314
Misha <20xim04@gmail.com>
2023-04-18 11:52:05 +0300

    mysql connect

--- include/database.h
+++ include/database.h
@@ -1,6 +1,11 @@
 #pragma once
 #include <iostream>
 #include <vector>
+#include "mysql_connection.h"
+#include <cppconn/driver.h>
+#include <cppconn/exception.h>
+#include <cppconn/resultset.h>
+#include <cppconn/statement.h>
 struct version{
     std::string sv_local_version;
     std::string cl_version;
@@ -13,8 +18,30 @@ struct version{
 };
 class database {
  private:
+    sql::Driver *driver;
+    sql::Connection *con;
+    sql::Statement *stmt;
  public:
-  database() {}
+  database() {
+    
+
+    /* Create a connection */
+    driver = get_driver_instance();
+   
+    try{
+      con = driver->connect("tcp://192.168.0.104:3306", "test", "1");
+      con->setSchema("pc_club");
+      stmt = con->createStatement();
+    }catch (sql::SQLException &e) {
+      std::cout << "# ERR: SQLException in " << __FILE__;
+      std::cout << "(" << __FUNCTION__ << ") on line "
+        << __LINE__ << std::endl;
+      std::cout << "# ERR: " << e.what();
+      std::cout << " (MySQL error code: " << e.getErrorCode();
+      std::cout << ", SQLState: " << e.getSQLState() << " )" << std::endl;
+    }
+    
+  }
   std::vector<version> sv_local_getversions() {
     std::vector<version> versions;
     versions.push_back(version("0.0.3","0.0.1<=0.0.5","0.0.1"));
@@ -33,5 +60,8 @@ class database {
     versions.push_back(version("0.0.8","0.0.4<=0.0.6","0.0.1"));
     return versions;
   }
-  ~database() {}
+  ~database() {
+    delete stmt;
+    delete con;
+  }
 };
